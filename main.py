ad3a08ebd19d05eb15e073c7199d997f8626983b
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2025-05-29 13:39:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import matplotlib.ticker as mticker # Importar para formatear el eje Y
 
 st.set_page_config(layout="wide")
